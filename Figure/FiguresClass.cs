1a6913d5f5caa098cc6b004b4964dba779a7df5a
GodOfTea <lomonosov66@mail.ru>
2019-02-12 19:55:03 +0500

    v0.2

---
+++ Figure/FiguresClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Figures
+{
+    public abstract class FiguresClass
+    {
+        public int Width;
+        public int Height;
+        public int[] Color;
+        public int Position;
+    }
+
+    public class Rectangles : FiguresClass
+    {
+        //
+    }
+
+    public class Cubes : FiguresClass
+    {
+        public int XLocation;
+        public int YLocation;
+        public string Name;
+        public int Speed;
+    }
+}
--- Game/Form1.cs
+++ Game/Form1.cs
@@ -1,53 +1,68 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using Figures;
 
 namespace Game
 {
     public partial class mainForm : Form
     {
         Random rnd = new Random();
+        Rectangles Player = new Rectangles();
+        Cubes Cube = new Cubes();
+        PictureBox PlayerPB = new PictureBox();
+
         static public int NumberOfSCubes = 6;
-        public int Score = 0;
+        static public int Score = 0;
 
         public int[] CubeSpeed = new int[NumberOfSCubes];
         public PictureBox[] Cubes = new PictureBox[NumberOfSCubes];
 
-        //The boundaries of the cubes
-        //public int CollisionCheck(int rndNew)
-        //{
-        //    bool flag = true;
-        //    while (flag)
-        //    {
-        //        flag = false;
-        //        for (int i = 0; i < NumberOfSCubes - 1; i++)
-        //        {
-        //            for (int j = 1; j < NumberOfSCubes; j++)
-        //            {
-        //                if ((Cubes[i].Location.X >= Cubes[j].Location.X && Cubes[i].Location.X <= Cubes[j].Location.X + 50) ||
-        //                    (Cubes[i].Location.X + 50 >= Cubes[j].Location.X && Cubes[i].Location.X + 50 <= Cubes[j].Location.X + 50))
-        //                {
-        //                    rndNew = rnd.Next(playground.Left, playground.Right - 50);
-        //                    flag = true;
-        //                }
-        //            }
-        //        }
-        //    }
-        //    return rndNew;
-        //}
+        public PictureBox CreatingFigure (string figure, int order)
+        {
+            PictureBox figureCopy = new PictureBox();
+            switch (figure)
+            {
+                case "player":
+                    Player.Width = 300;
+                    Player.Height = 50;
+                    Player.Color = new int[3];
+                    Player.Color[0] = 0;
+                    Player.Color[1] = 0;
+                    Player.Color[2] = 192;
+                    Player.Position = playground.Bottom - (playground.Bottom / 5);
+                    break;
+                case "cube":
+                    int randomValue = rnd.Next(playground.Left, playground.Right - 50);
+                    Cube.Width = 50;
+                    Cube.Height = 50;
+                    Cube.Color = new int[3];
+                    Cube.Color[0] = rnd.Next(150, 255);
+                    Cube.Color[1] = rnd.Next(150, 255);
+                    Cube.Color[2] = rnd.Next(150, 255);
+                    Cube.Position = playground.Top - 30;
+                    Cube.XLocation = randomValue;
+                    Cube.YLocation = 0;
+                    Cube.Name = "ccccube";
+                    Cube.Speed = rnd.Next(4, 10);
+                    figureCopy = CreatingCubs(order, figureCopy);
+                    break;
+                default:
+                    break;
+            }
+            return figureCopy;
+        }
 
-        public PictureBox CreatingCubs (int order)
+        public PictureBox CreatingCubs (int order, PictureBox cubeCopy)
         {
             int randomValue = rnd.Next(playground.Left, playground.Right - 50);
-            PictureBox cubeCopy = new PictureBox();
-            cubeCopy.BackColor = cube.BackColor;
-            cubeCopy.Location = new Point(randomValue, 0);
-            cubeCopy.Width = 50;
-            cubeCopy.Height = 50;
+            cubeCopy.Location = new Point(Cube.XLocation, Cube.YLocation);
+            cubeCopy.Width = Cube.Width;
+            cubeCopy.Height = Cube.Height;
+            cubeCopy.Name = Cube.Name;
+            CubeSpeed[order] = Cube.Speed;
+            cubeCopy.BackColor = Color.FromArgb(Cube.Color[0], Cube.Color[1], Cube.Color[2]);
             Controls.Add(cubeCopy);
-            cubeCopy.Name = "cube" + order;
-            CubeSpeed[order] = rnd.Next(4, 10);
-            cubeCopy.BackColor = Color.FromArgb(rnd.Next(150, 255), rnd.Next(150, 255), rnd.Next(150, 255));
             cubeCopy.BringToFront();
             return cubeCopy;
         }
@@ -62,12 +77,14 @@ namespace Game
             TopMost = true;
             Bounds = Screen.PrimaryScreen.Bounds;
 
-            player.Top = playground.Bottom - (playground.Bottom / 5); //player pos
-            cube.Top = playground.Top - 30; //cube start pos
+            PlayerPB = CreatingFigure("player", -1);
+
+            player.Top = Player.Position; //player pos
+            cube.Top = Cube.Position; //cube start pos
 
             for (int i = 0; i < NumberOfSCubes; i++)
             {
-                Cubes[i] = CreatingCubs(i);
+                Cubes[i] = CreatingFigure("cube", i);
             }
         }
 
@@ -83,14 +100,14 @@ namespace Game
                 if (Cubes[i].Bottom >= player.Top && Cubes[i].Bottom <= player.Bottom && Cubes[i].Left >= player.Left && Cubes[i].Right <= player.Right)
                 {
                     Cubes[i].Dispose();
-                    Cubes[i] = CreatingCubs(i);
+                    Cubes[i] = CreatingFigure("cube", i);
                     Score += 1;
                     points_label.Text = Score.ToString();
                 }
                 if (Cubes[i].Bottom >= playground.Bottom)
                 {
                     Cubes[i].Dispose();
-                    Cubes[i] = CreatingCubs(i);
+                    Cubes[i] = CreatingFigure("cube", i);
                 }
             }
         }
